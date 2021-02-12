8c156790e97b2ef2c6ffee662eb173ef7f8e3240
Justin Mcglamry <mcglamryjustin9@gmail.com>
2021-02-11 23:46:12 -0500

    format change

--- Jenga/array1.js
+++ Jenga/array1.js
@@ -1,24 +1,23 @@
-var btn = document.getElementById('btn')
-const btn2 = document.getElementById('btn2')
+var btn = document.getElementById('btn');
+const btn2 = document.getElementById('btn2');
+var i = 0;
+var arr = [0];
 
-var i = 0
+function storeData(){
+    var input = document.getElementById('input').value;
 
-var arr = [0]
- function storeData(){
-var input = document.getElementById('input').value
-
-var arr2 = [0]
-arr[i] = input
-i++
-
-
+    var arr2 = [0];
+    arr[i] = input;
+    i++;
 }
+
 function compare(a,b){
-return a-b
+    return a-b;
+
 }
 function minAndMax(){
- arr.sort(compare)
- const p = document.getElementById('para')
- p.innerText = "Min Value is " + arr[0] + " and Max Value is " + arr[arr.length-1]
-
+    arr.sort(compare);
+    const p = document.getElementById('para');
+    p.innerText = "Min Value is " + arr[0] + " and Max Value is " + arr[arr.length-1];
+ 
 }
